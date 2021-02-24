750d7b94b103a957721d2c355222ae8d30b75de4
jw910731 <jw910731@gmail.com>
2021-02-25 01:10:09 +0800

    [portable] Make screen rotate more portable
    Improve screen rotate support for i3

--- dot_shell/executable_screen_rotate.py
+++ dot_shell/executable_screen_rotate.py
@@ -5,6 +5,7 @@
 import re
 import subprocess
 import sys
+import os
 
 DISPLAY = "DisplayPort-0"
 SN_ID = "61L0194501Q"
@@ -26,14 +27,40 @@ def ddc_get_rotate(monitor):
     return rawState
 
 
-def set_rotate(side):
+def set_rotate(side, display):
     # xrandr --output DisplayPort-0 --rotate left
-    subprocess.run(['xrandr', '--output', DISPLAY, '--rotate', side])
+    subprocess.run(['xrandr', '--output', display, '--rotate', side])
+
+
+def query_rotation(display):
+    result = subprocess.run(
+        ['/bin/bash', '-c', """xrandr --query --verbose | grep "DisplayPort-0" | cut -d ' ' -f 6"""], stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        return None
+    else:
+        return result.stdout.decode('ascii')
+
+
+def get_desktop_session():
+    result = subprocess.run(['/bin/bash', '-c',
+                             """w $(id -un) -f | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $8; exit}'"""], stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        return None
+    else:
+        return result.stdout.decode('ascii')
+
+
+def reload_wallpaper():
+    if 'i3' in get_desktop_session():
+        subprocess.run(['nitrogen', '--restore'],
+                       stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
 
 def main():
     state = ddc_get_rotate(SN_ID)
-    set_rotate(MAPPING[state])
+    if MAPPING[state] != query_rotation:
+        set_rotate(MAPPING[state], DISPLAY)
+        reload_wallpaper()
 
 
 if __name__ == '__main__':
