71dafcfe2c15ae2d3c9a4e4d515477875f668b65
jw910731 <jw910731@gmail.com>
2021-04-02 19:29:20 +0800

    Change screen auto rotate run shell to sh

--- dot_shell/screen_helper.sh
+++ dot_shell/screen_helper.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 for i in {1..6}; do env DISPLAY=$(w -f $(id -un) | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}') XAUTHORITY=$HOME/.Xauthority $HOME/.shell/screen_rotate.py; sleep 10; done;
