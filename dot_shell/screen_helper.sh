674b26e17812d747025eb4b060fc8154f6f29f91
jw910731 <jw910731@gmail.com>
2022-09-29 15:39:33 +0800

    [Xorg] Remove screen rotate helper

--- dot_shell/screen_helper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-for i in {1..6}; do env DISPLAY=$(w -f $(id -un) | awk 'NF > 7 && $2 ~ /tty[0-9]+/ {print $3; exit}') XAUTHORITY=$HOME/.Xauthority $HOME/.shell/screen_rotate.py; sleep 10; done;
